ca78d5bf81359d3464c80477e2e72938623376eb
oscar-sinaga <you@example.com>
2023-12-19 22:39:47 +0700

    initialize git

---
+++ README.md
@@ -0,0 +1,267 @@
+# Description Model
+Machine Learning untuk deteksi sentimen aspek pada review hotel
+Aspek yang ditentukan adalah sbb:
+1. Surrounding (surrounding_sentiment atau aspect-surrounding)
+2. Service (service_sentiment atau aspect-service)
+3. Meal (Meal_sentiment atau aspect-meal)
+4. Location (location_sentiment atau aspect-location)
+5. Staff (staff_sentiment atau aspect-staff)
+6. Room (room_sentiment atau aspect-room)
+7. Facility (facility_sentiment atau aspect-facility)
+8. Quality (quality_sentiment atau aspect-quality)
+9. Value (value_sentiment atau aspect-value)
+
+Sebelum ditrain dengan model machine learning, data teks (_Features_) nya perlu di cleaning terlebih dahulu seperti 
+- Menon-kapitalkan semua teks
+- Mengganti baris baru dengan spasi
+- Menghapus tanda kutip (') 
+- Mengganti strip (-) dengan spasi
+- Mengganti yang bukan huruf kecil dengan spasi
+- Menghapus semua _stopwords_
+- _Steming_ semua kata dalam teks.
+
+Data labelnya juga perlu dibakukan ke 3 label yaitu `positive`, `neutral`, dan `negative`. 
+
+Setelah semua teks berhasil dicleaning, kemudian teksnya perlu divektorisasi yaitu mengubah teks menjadi bentuk yang mudah dipahami oleh mesin yaitu vektor, susunan dari angka-angka bilangan bulat. Teknik yang digunakan untuk vektorisasi teks di model ini adalah dengan _TF-IDF (Term Frequency-Inverse Document Frequency)_ dengan suatu class Python:
+
+        vectorizer = TfidfVectorizer()
+
+Model machine learning ini terbagi menjadi 2 yaitu model untuk sentimen bahasa indonesia dan model untuk sentimen bahasa inggris.
+
+# Developement
+
+Seperti yang kita lihat di atas, ada beberapa metrik evaluasi yang sangat rendah seperti F1-Score dan recall yang sangat kecil pada class negatif. Oleh karena itu disini ada beberapa development model pada folder `dev`. 
+
+## Ruled Based
+Pada developedment ini kita akan melakukan developement terutama pada saat model melakukan inferencing atau prediction. Disini kita akan menambahkan metode rule-based selain menggunakan machine learning untuk melakukan inferencing atau prediction sentiment dari suatu teks.
+
+Konsepnya rule basednya adalah kita terlebih dahulu mempunyai dataframe keyword positif dan negatif pada setiap aspek. Kemudian dari sebuah teks, model akan mencari apakah di dalam teks tersebut ada keyword positif atau negatif di setiap aspeknya. Kemudian dari jumlah keyword positif dan negatif akan dihitung scorenya. Scorenya akan dihitung dengan cara jumlah kemunculan Keyword positif akan dikurangin dengan jumlah kemunculan keyword negatif pada setiap aspek. jika score 0, maka modelnya akan menggunakan machine learning yang sudah dilatih untuk melakukan prediksi. Jika scorenya lebih dari 0 maka teks akan diprediksi positif, jika kurang dari 0, maka teks akan diprediksi positif.
+
+Prediksi dengan menggunakan rule based ini merupakan hal optional, bisa tidak menggunakan keyword dalam melakukan prediksi.
+
+### Algoritmanya
+1. Pertama perlu membaca dataframe keyword per aspect terlebih dahulu
+2. Kemudian, teks nya perlu dihitung scorenya setiap aspek berdasarkan  kemunculan keyword positif dan negatif setiap aspeknya pada teks. yaitu $$ScoreAspect = JumlahKeywordPositif-JumlahKeywordNegatif$$   
+3. Setelah itu, dilooping per aspek-nya, jika $ScoreAspect>0$ maka sentimen akan diberikan positif jika  $ScoreAspect<0$ maka sentimen teks langsung dibuat negatif pada aspek tersebut.
+4. Jika $ScoreAspect=0$, maka prediksi sentimen pada teks akan dilakukan oleh machine learning yang sudah dilatih.
+
+## Machine Learning
+
+Selain itu disini ada modifikasi sedikit di modelnya. yaitu:
+1. Menggabungkan (ensembel) 4 model yaitu `LinearSVC`, `RandomForestClassifier`, `LogisticRegression`, `GradientBoostingClassifier` berdasarkan voting terbanyak (`hard`).
+2. Menggunakan *cost-sensitive learning* yaitu `class_weight = 'balanced'`  untuk mengatasi data yang sangat tidak balanced apalagi pada class negatif yang sangat sedikit. konsepnya adalah bobot pelatihan pada label atau *class* yang datanya ;lebih besar sedikit akan diperbesar menyesuaikan data *class* yang lebih banyak.
+
+```py
+# Initialize classifiers with balanced class weights and set a random seed for reproducibility
+svm = LinearSVC(class_weight=class_weight, dual='auto', random_state=42)
+rf = RandomForestClassifier(class_weight=class_weight, random_state=42)
+lr = LogisticRegression(multi_class='multinomial', class_weight=class_weight, random_state=42)
+gb = GradientBoostingClassifier(random_state=42)
+
+# Combine all models using Voting Classifier
+ensemble = VotingClassifier(estimators=[('lr', lr), ('svm', svm), ('rf', rf), ('gb', gb)], voting='hard', n_jobs=-1)
+```
+
+# Evaluation
+## **Model Bahasa Inggris**
+ ### **1. aspect-surrounding**
+        
+- Accuracy
+        
+         65.0 %
+
+- classification_report
+
+                        precision    recall  f1-score   support
+
+        negative        0.46      0.48      0.47       133
+        neutral         0.47      0.46      0.46       415
+        positive        0.75      0.76      0.75       972
+
+        accuracy                            0.65      1520
+        macro avg       0.56      0.56      0.56      1520
+        weighted avg    0.65      0.65      0.65      1520
+
+
+- confusion_matrix
+
+![confusion_matrix_surrounding](image/confusion_matrix/english/surrounding.png)
+
+
+### **2. aspect-service**
+* Accuracy
+ 
+        75.0657894736842 %
+
+* classification_report
+               
+                        precision    recall  f1-score   support
+
+        negative          0.43      0.45      0.44        93
+        neutral           0.72      0.75      0.73       579
+        positive          0.82      0.78      0.80       848
+
+        accuracy                              0.75      1520
+        macro avg         0.65      0.66      0.66      1520
+        weighted avg      0.75      0.75      0.75      1520
+
+
+* confusion_matrix
+
+![confusion_matrix_service](image/confusion_matrix/english/service.png)
+### **3. aspect-meal**
+- Accuracy
+ 
+       78.8157894736842 %
+
+- classification_report
+               
+                        precision    recall  f1-score   support
+
+        negative          0.29      0.24      0.26        70
+        neutral           0.83      0.84      0.83       794
+        positive          0.79      0.78      0.78       656
+
+        accuracy                               0.79      1520
+        macro avg          0.63      0.62      0.63      1520
+        weighted avg       0.78      0.79      0.79      1520
+
+
+
+- confusion_matrix
+![confusion_matrix_meal](image/confusion_matrix/english/meal.png)
+
+### **4. aspect-location**
+- Accuracy
+ 
+        84.47368421052632 %
+
+- classification_report
+               
+                        precision    recall  f1-score   support
+
+        negative          0.00      0.00      0.00        23
+        neutral           0.85      0.91      0.88       878
+        positive          0.84      0.78      0.81       619
+
+        accuracy                              0.84      1520
+        macro avg         0.57      0.56      0.56      1520
+        weighted avg      0.84      0.84      0.84      1520
+
+
+- confusion_matrix
+ 
+![confusion_matrix_location](image/confusion_matrix/english/location.png)
+
+
+
+### **5. aspect-staff**
+
+- Accuracy
+ 
+        86.90789473684211 %
+
+- classification_report
+               
+                        precision    recall  f1-score   support
+
+        negative           0.59      0.38      0.46        61
+        neutral            0.85      0.90      0.88       636
+        positive           0.90      0.88      0.89       823
+
+        accuracy                               0.87      1520
+        macro avg          0.78      0.72      0.74      1520
+        weighted avg       0.87      0.87      0.87      1520
+
+
+- confusion_matrix
+ ![confusion_matrix_staff](image/confusion_matrix/english/staff.png)
+
+### **6. aspect-room**
+- Accuracy
+ 
+        76.18421052631578 %
+
+- classification_report
+                
+                        precision    recall  f1-score   support
+
+        negative           0.43      0.43      0.43        92
+        neutral            0.80      0.80      0.80       762
+        positive           0.76      0.76      0.76       666
+
+        accuracy                               0.76      1520
+        macro avg          0.66      0.67      0.66      1520
+        weighted avg       0.76      0.76      0.76      1520
+
+- confusion_matrix
+  ![confusion_matrix_room](image/confusion_matrix/english/room.png)
+
+### **7. aspect-facility**
+- Accuracy
+ 
+        73.75 %
+
+- classification_report
+              
+                         precision    recall  f1-score   support
+
+        negative           0.51      0.61      0.56       171
+        neutral            0.79      0.87      0.82       935
+        positive           0.72      0.50      0.59       414
+
+        accuracy                               0.74      1520
+        macro avg          0.67      0.66      0.66      1520
+        weighted avg       0.74      0.74      0.73      1520
+
+
+- confusion_matrix
+  ![confusion_matrix_facility](image/confusion_matrix/english/facility.png)
+
+
+### **8. aspect-quality**
+- Accuracy
+ 
+        82.56578947368422 %
+
+
+- classification_report
+               
+                         precision    recall  f1-score   support
+
+        negative           0.29      0.14      0.19        35
+        neutral            0.84      0.98      0.90      1258
+        positive           0.59      0.06      0.10       227
+
+        accuracy                               0.83      1520
+        macro avg          0.57      0.39      0.40      1520
+        weighted avg       0.79      0.83      0.77      1520
+
+
+
+- confusion_matrix
+  ![confusion_matrix_quality](image/confusion_matrix/english/quality.png)
+
+
+### **9. aspect-value**
+- Accuracy
+
+        82.56578947368422 %
+
+
+- classification_report
+               
+                        precision    recall  f1-score   support
+
+        negative           0.38      0.29      0.32        42
+        neutral            0.85      0.95      0.90      1180
+        positive           0.70      0.42      0.52       298
+
+        accuracy                               0.83      1520
+        macro avg          0.64      0.55      0.58      1520
+        weighted avg       0.81      0.83      0.81      1520
+
+
+
+- confusion_matrix
+   ![confusion_matrix_value](image/confusion_matrix/english/value.png)
---
+++ hotel_aspect/dev_hotel_aspect.py
@@ -0,0 +1,423 @@
+# -*- coding: utf-8 -*-
+import joblib
+import numpy as np
+import re
+import os
+from nltk.stem import PorterStemmer
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import  LinearSVC
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+import seaborn as sns
+from sklearn.metrics import (accuracy_score,
+                             classification_report,
+                             confusion_matrix)
+import matplotlib.pyplot as plt
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+# PATH = os.path.abspath(os.path.join(__file__, "./../"))
+PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+
+
+class HotelAspect:
+    def __init__(self, dirpath=PATH):
+        """
+        Initialize the HotelAspect class.
+
+        Parameters:
+        - dirpath (str): Path to the directory containing necessary files.
+        """
+        self.dirpath = dirpath
+        self.models = None
+        self.vectorizers = None
+        self.regex = re.compile('<.*?>')
+        self.aspect_keywords = self.load_aspect_keywords()
+
+        self.load_dict(os.path.join(dirpath, 'data','dict'))
+
+        self.aspects = ["surrounding", "service", "meal", "location",
+        "staff", "room", "facility", "quality", "value"]
+        self.stemmer = PorterStemmer()
+        self.load_models(dirpath)
+
+    def get_classes(self):
+        """
+        Get classes from the model.
+
+        Returns:
+        - classes (list): List of classes from the model.
+        """
+
+        return self.model.classes_
+    
+    def load_aspect_keywords(self,data = 'data/hotel-aspect-keywords.xlsx',sheet_name = 'english_positive'):
+        """
+        Load aspect keywords for helping inference.
+
+        Parameters:
+        - data (str): Path to the Excel file containing aspect keywords.
+
+        Returns:
+        - aspect_keywords (dict): Dictionary of aspect keywords.
+        """
+
+        aspect_keywords = pd.read_excel(data,sheet_name=sheet_name).to_dict(orient='list')
+        aspect_keywords = {key: [value for value in values if pd.notna(value)] for key, values in aspect_keywords.items()}
+        return aspect_keywords
+
+
+    def load_models(self, dirpath = PATH, save_path = "models_english"):
+        """
+        Load models and vectorizers for each aspect.
+
+        Parameters:
+        - dirpath (str): Path to the directory containing models and vectorizers.
+        """
+
+        self.models = {}
+        self.vectorizers = {}
+
+        # Iterate through each aspect and load the corresponding model and vectorizer
+        for aspect in self.aspects:
+            # Construct file paths for vectorizer and classifier
+            vect = f"tfidf-{aspect}"
+            clf = f"clf-{aspect}"
+
+            # Load the TF-IDF vectorizer
+            model_tfidf = joblib.load(os.path.join(dirpath,save_path, vect))
+
+            # Load the classifier model
+            model_clf = joblib.load(os.path.join(dirpath, save_path, clf))
+
+            # Store the loaded models and vectorizers in the class attributes
+            self.models[aspect] = model_clf
+            self.vectorizers[aspect] = model_tfidf
+
+    def train_test(self,texts,sentiments, with_score=True,class_weight='balanced'):
+        """
+        Train and test the model.
+
+        Parameters:
+        - texts (pd.Series): Text data for training.
+        - sentiments (pd.DataFrame): Sentiments corresponding to each aspect.
+        - with_score (bool) : If true, the model will use score_aspect for inferencing
+        - class_weight : dict or 'balanced', or None, default='balanced'
+                         Weights associated with classes in the form {class_label: weight}. 
+                         If not given, all classes are supposed to have weight balanced
+        """
+
+        self.aspects = sentiments.columns
+        texts = texts.apply(self.clean_text)
+               
+        # Iterate through all aspects for training and testing per aspect
+        for aspect in self.aspects:
+            print(f'{aspect} aspect')
+            # Split data into training and testing sets
+            X_train, X_test, y_train, y_test = train_test_split(texts, sentiments[aspect], test_size=0.1, stratify=sentiments[aspect], random_state=42)
+            
+            # Initialize TF-IDF vectorizer
+            vectorizer = TfidfVectorizer()
+            X_train_vect = vectorizer.fit_transform(X_train)
+            
+            # Store the vectorizer for later use
+            self.vectorizers[aspect] = vectorizer
+            
+            
+            # Initialize classifiers with balanced class weights and set a random seed for reproducibility
+            svm = LinearSVC(class_weight=class_weight, dual='auto', random_state=42)
+            rf = RandomForestClassifier(class_weight=class_weight, random_state=42)
+            lr = LogisticRegression(multi_class='multinomial', class_weight=class_weight, random_state=42)
+            gb = GradientBoostingClassifier(random_state=42)
+
+            # Combine all models using Voting Classifier
+            ensemble = VotingClassifier(estimators=[('lr', lr), ('svm', svm), ('rf', rf), ('gb', gb)], voting='hard', n_jobs=-1)
+
+            # Fit the ensemble model to the training data
+            ensemble.fit(X_train_vect, y_train)
+
+            # Store the trained model in the class attribute
+            self.models[aspect] = ensemble
+
+            # Make predictions on the testing set and evaluate the model
+            y_pred = []
+            #if using score aspect for inference 
+            if with_score:
+                for text in X_test:
+                    score_aspect = self.score_text_on_aspect(text, self.aspect_keywords)
+                    y_pred.append(self.predict_aspect_with_score(text, aspect, score_aspect))
+            
+            #if not using score aspect for inference 
+            else :
+                for text in X_test:
+                    y_pred.append(self.predict_aspect(text, aspect))
+
+            # Evaluate the model on the testing set
+            self.evaluate_model(y_test, y_pred,aspect)
+
+    def create_models(self, texts, sentiments,with_score=True,save_path = "models_english"):
+        """
+        Train models and save them.
+
+        Parameters:
+        - texts (pd.Series): Text data for training.
+        - sentiments (pd.DataFrame): Sentiments corresponding to each aspect.
+        """
+
+        # Train and test models
+        self.train_test(texts, sentiments, with_score)
+
+        # Save the trained models
+        self.save_model(save_path=save_path)
+
+    def save_model_aspect(self, vectorizer, model, aspect,save_path = "models_english"):
+        """
+        Save the trained vectorizer and model for a specific aspect.
+
+        Parameters:
+        - vectorizer: Trained TF-IDF vectorizer.
+        - model: Trained machine learning model.
+        - aspect (str): Aspect for which the model is trained.
+
+        Example:
+        - save_model_aspect(vectorizer, model, 'location')
+        - Saved Files: 'tfidf-location', 'clf-location'
+        """
+
+        print(f'Saving model {aspect}')
+        joblib.dump(vectorizer, os.path.join(self.dirpath, f'{save_path}', "tfidf-%s" % aspect))
+        joblib.dump(model, os.path.join(self.dirpath, f'{save_path}', "clf-%s" % aspect))
+
+
+    def save_model(self,save_path = "models_english"):
+        """
+        Save all trained vectorizers and models for each aspect.
+
+        Example:
+        - save_model()
+        - Saved Files: 'tfidf-facility', 'clf-facility', 'tfidf-location', 'clf-location', ...
+        """
+
+        for aspect in self.aspects:
+            self.save_model_aspect(self.vectorizers[aspect], self.models[aspect], aspect,save_path=save_path)
+        print('Save model Finished')
+
+
+
+    def evaluate_model(self,y_test,y_pred,aspect):
+        """
+        Evaluate the model performance.
+
+        Parameters:
+        - y_test (list): True labels.
+        - y_pred (list): Predicted labels.
+        """
+        
+        #Lower every row in both label data
+        y_test = y_test.apply(lambda row: row.lower())
+
+        cm_labels = np.unique(y_test)
+
+        # Calculate confusion matrix
+        cm_array = confusion_matrix(y_test, y_pred)
+        cm_array_df = pd.DataFrame(cm_array, index=cm_labels, columns=cm_labels)
+
+        # Print accuracy, classification report, and visualize the confusion matrix
+        print("Accuracy\n", accuracy_score(y_test, y_pred) * 100, "%")
+        print("\nclassification_report\n", classification_report(y_test, y_pred))
+
+        # Visualize the confusion matrix using a heatmap
+        sns.heatmap(cm_array_df, annot=True, annot_kws={"size": 12}, cmap="crest", fmt='d')
+        plt.title(f'Confusion matrix for {aspect} aspect')
+        plt.xlabel(f'Prediction')
+        plt.ylabel(f'True')
+        plt.show()
+
+
+    def load_dict(self, dict_path):
+        """
+        Load English stopwords from a specified file.
+
+        Parameters:
+        - dict_path (str): The path to the directory containing the stopwords file.
+
+        """
+
+        with open(os.path.join(dict_path, 'en_stopwords.txt'), 'r') as f:
+            en_stopwords = f.read().splitlines()
+        
+        # Store the loaded stopwords in the class attribute
+        self.stopwords = en_stopwords
+
+    def clean_html(self, raw_text):
+        """
+        Clean HTML tags from the raw text.
+
+        Parameters:
+        - raw_text (str): Raw text containing HTML tags.
+
+        Returns:
+        - cleantext (str): Text without HTML tags.
+        """
+
+        cleantext = re.sub(self.regex, '', raw_text)
+        return cleantext
+
+    def clean_text(self, text):
+        """
+        Clean and preprocess the input text.
+
+        Parameters:
+        - text (str): Input text.
+
+        Returns:
+        - temp (str): Processed text.
+        """
+
+        temp = str(text).lower()
+        temp = re.sub('\n', " ", temp)
+        temp = re.sub('\'', "", temp)
+        temp = re.sub('-', " ", temp)
+        temp = re.sub("[^a-z]", " ", str(temp))
+        temp = temp.split()
+        temp = [w for w in temp if not w in self.stopwords]
+        temp = " ".join(word for word in temp)
+        temp = " ".join([self.stemmer.stem(word) for word in temp.split()])
+
+        return temp
+
+    def preprocessing(self, name, desc):
+        """
+        Preprocess the input name and description.
+
+        Parameters:
+        - name (str): Hotel name.
+        - desc (str): Hotel description.
+
+        Returns:
+        - text (str): Processed text.
+        """
+
+        text = self.clean_text(name) + " "+self.clean_text(desc)
+        return text
+
+    def predict_aspect_with_score(self, text, aspect, score):
+        """
+        Predict the sentiment for a given aspect if score given.
+
+        Parameters:
+        - text (str): Input text to predict sentiment for.
+        - aspect (str): The aspect for which sentiment is predicted.
+        - score (dict): Aspect scores based on keywords.
+
+        Returns:
+        - prediction (str or np.nan): Predicted sentiment ('Positive', 'Negative', or np.nan if not applicable).
+        """
+        # Clean the input text
+        text = self.clean_text(text)
+
+        # Check if the aspect score is positive, negative, or neutral
+        if score[aspect] > 0:
+            return 'positive'
+        elif score[aspect] < 0:
+            return 'negative'
+        else:
+            # If score = 0, use the trained model to make a prediction
+           pred = self.predict_aspect(text,aspect)
+           return pred
+        
+    def predict_aspect(self,text,aspect):
+        """
+        Predict the sentiment for a given aspect if score is not given.
+
+        Parameters:
+        - text (str): Input text to predict sentiment for.
+        - aspect (str): The aspect for which sentiment is predicted.
+
+        Returns:
+        - prediction (str or np.nan): Predicted sentiment ('Positive', 'Negative', or np.nan if not applicable).
+        """
+
+        vectorized = self.vectorizers[aspect].transform([text])
+        pred = self.models[aspect].predict(vectorized)
+
+        # Check for special case where prediction is "na"
+        if pred[0] == "na":
+            # if the pred is Nan, then the sentiment is neutral
+            return 'neutral'
+        else:
+            return pred[0].lower()
+
+
+    def predict(self, name, desc, with_score=True):
+        """
+        Predict sentiments for all aspects.
+
+        Parameters:
+        - name (str): Hotel name.
+        - desc (str): Hotel description.
+
+        Returns:
+        - result (dict): Predicted sentiments for each aspect.
+        """
+
+        #text = self.preprocessing(name, desc)
+        score_aspect = self.score_text_on_aspect(name+""+desc,self.aspect_keywords)
+
+        result = {}
+
+        # Iterate through predefined aspects
+        if with_score:
+            for aspect in self.aspects:
+            # Predict sentiment for each aspect
+                result[aspect] = self.predict_aspect_with_score(name + ' ' + desc, aspect, score_aspect)
+        else:
+            for aspect in self.aspects:
+            # Predict sentiment for each aspect
+                result[aspect] = self.predict_aspect(name + ' ' + desc, aspect)
+        return result
+    
+    def score_text_on_aspect(self,text, aspect_keywords):
+        """
+        Score the input text based on aspect keywords.
+
+        Parameters:
+        - text (str): Input text.
+        - aspect_keywords (dict): Aspect keywords.
+
+        Returns:
+        - aspect_value (dict): Aspect scores.
+
+        Example:
+        - Input: score_text_on_aspect("Great location and friendly staff", aspect_keywords)
+        - Output: {'location': 1, 'staff': 1}
+        """
+    
+        # Convert text to lowercase for case-insensitive matching
+        lower_text = text.lower()
+
+        # Initialize dictionaries to store aspect sentiment and scores
+        aspect_sentiment = {}
+        aspect_value = {}
+
+        # Iterate through predefined aspects
+        for aspect, keywords in aspect_keywords.items():
+            aspect_sentiment[f'{aspect}'] = 0
+
+            # Count occurrences of keywords in the text
+            for keyword in keywords:
+                if keyword.lower() in lower_text:
+                    aspect_sentiment[f'{aspect}'] += 1
+                else:
+                    pass
+
+            # Store score per aspect
+            if aspect.split('_')[1] == 'negative':
+                aspect_name = aspect.split('_')[0]
+                aspect_value[f'{aspect_name}'] = aspect_sentiment[f'{aspect_name}_positive'] - aspect_sentiment[f'{aspect_name}_negative']
+
+        return aspect_value
